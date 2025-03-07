01f9aee2cfef393ab6fdb7795b18881c0c153beb
Dipika Bhagora <dpkabhagora006@gmail.com>
2025-03-08 00:20:37 +0530

    Database connected

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+const express = require("express")
+const mongoose = require("mongoose")
+
+//express obj
+const app = express()
+app.use(express.json()) //to accept data as json
+
+mongoose.connect("mongodb://localhost:27017/25_node_internship").then(() =>{
+    console.log("database connected...")
+})
+
+//server connnection
+const PORT = 3000
+app.listen(PORT,()=>{
+    console.log("server started on port number ",PORT)
+})
